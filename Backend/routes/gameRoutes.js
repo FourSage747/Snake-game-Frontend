83179fc71374c73629e4044db7ad32178c6f3c50
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-07-07 18:03:35 +0200

    test

--- Backend/routes/gameRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getRecords, addRecord } = require("../controllers/gameController");
-const router = express.Router();
-
-router.get("/records", getRecords);
-router.post("/records", addRecord);
-
-module.exports = router;
